9f75933c1d788de9cb2f946160963de29d3e84e6
Rick Gibbed <rick@gibbed.us>
2021-04-10 16:55:22 -0500

    Bump version to 1.0.1.

--- projects/Gibbed.CozyGrove.ToggleHUD/AssemblyInfo.cs
+++ projects/Gibbed.CozyGrove.ToggleHUD/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: MelonInfo(
     typeof(Gibbed.CozyGrove.ToggleHUD.MyMod),
     "Gibbed.CozyGrove.ToggleHUD",
-    "1.0.0",
+    "1.0.1",
     "gibbed",
     "https://github.com/gibbed/CozyGrove-Mods")]
 [assembly: MelonGame("Spry Fox", "Cozy Grove")]
